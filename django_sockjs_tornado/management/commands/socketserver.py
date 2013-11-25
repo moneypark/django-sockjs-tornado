009970d2da424cae6fe5c3e5cebe3f9bd29ca7b4
Volodymyr Pavlenko <mindinpanic@gmail.com>
2013-11-25 10:55:30 +0200

    Add ssl options. Refactor code.

--- django_sockjs_tornado/management/commands/socketserver.py
+++ django_sockjs_tornado/management/commands/socketserver.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 from operator import add
 from optparse import make_option
@@ -27,36 +28,69 @@ class Command(BaseCommand):
             help='Set no_keep_alive on the connection if your server needs it')
     )
 
-    def handle(self, **options):
+    def check_settings(self):
         from django.core.exceptions import ImproperlyConfigured
-        if not getattr(settings, 'SOCKJS_CLASSES', None) or not getattr(settings, 'SOCKJS_CHANNELS', None):
-            raise ImproperlyConfigured("Can't find SOCKJS_CLASSES or SOCKJS_CHANNELS")
+
+        if not getattr(settings, 'SOCKJS_CONNECTIONS', None):
+            raise ImproperlyConfigured("Can't find SOCKJS_CONNECTIONS")
+
+        if settings.DEBUG:
+            return
+
+        # all next settings should be in none-DEBUG mode
+
+        if not getattr(settings, 'SOCKJS_SSL', None):
+            raise ImproperlyConfigured("SOCKJS_SSL isn't set in non-DEBUG mode")
+
+        if not settings.SOCKJS_SSL.get('certfile') or not settings.SOCKJS_SSL.get('keyfile'):
+            raise ImproperlyConfigured("SOCKJS_SSL should be dict with nonempty keys 'certfile' and 'keyfile'")
+
+    def build_urls(self):
         routers = []
-        for sockjs_class, channel_name in zip(settings.SOCKJS_CLASSES, settings.SOCKJS_CHANNELS):
+        for sockjs_class, channel_name in settings.SOCKJS_CONNECTIONS:
             module_name, cls_name = sockjs_class.rsplit('.', 1)
             module = import_module(module_name)
-            cls = getattr(module, cls_name)
+
             if not channel_name.startswith('/'):
                 channel_name = '/%s' % channel_name
 
-            routers.append(SockJSRouter(cls, channel_name))
-
-        app_settings = {
-            'debug': settings.DEBUG,
-        }
-
-        PORT = int(options['port'])
+            routers.append(SockJSRouter(getattr(module, cls_name), channel_name))
 
         urls = reduce(add, [r.urls for r in routers])
 
         if not urls:
-            sys.exit("Can't find any class in SOCKJS_CLASSES")
+            sys.exit("Can't find any class in SOCKJS_CONNECTIONS")
+        return urls
+
+
+    def build_application(self, urls, port):
+        app_settings = {
+            'debug': settings.DEBUG,
+        }
+
+        if not settings.DEBUG:
+            app_settings.update({
+                'ssl_options': {
+                    'certfile': settings.SOCKJS_SSL.get('certfile'),
+                    'keyfile': settings.SOCKJS_SSL.get('keyfile')
+                }
+            })
 
         app = web.Application(urls, **app_settings)
 
-        app.listen(PORT, no_keep_alive=options['no_keep_alive'])
+        app.listen(port, no_keep_alive=options['no_keep_alive'])
 
-        logger.info("Running sock app on port %s", PORT)
+
+
+    def handle(self, **options):
+        self.check_settings()
+
+        urls = self.build_urls()
+        port = int(options['port'])
+
+        self.build_application(urls, port)
+
+        logger.info("Running sock app on port %s", port)
         try:
             ioloop.IOLoop.instance().start()
         except KeyboardInterrupt:
